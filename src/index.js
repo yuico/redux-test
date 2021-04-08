edf0bdca095d439a43f727a029c2321e7dd4d9d6
yuikim <nokdoo00@gmail.com>
2021-04-08 18:03:21 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//STORE: Globalized state
+
+//ACTION: INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//REDUCER
+const counter = (state = 0,action) => {
+  switch(action.type){
+    case "INCREMENT": 
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
