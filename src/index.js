cc74f45bce126976cc0cbdf533248d3d715e7fa0
yuikim <nokdoo00@gmail.com>
2021-04-08 22:05:13 -0400

    use Provider of react-react for passing store globally

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
 /* const store = createStore(counterReduder, isLoggedReducer); */
 const store = createStore(
@@ -13,9 +14,9 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
