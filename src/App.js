38a7f69177e836dbecc02c4e7e19b8498f4cc87d
yuikim <nokdoo00@gmail.com>
2021-04-08 22:48:48 -0400

    add payload of action

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>counter {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
     </div>
